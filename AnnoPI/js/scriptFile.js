ab2587dfc9c35dcc479c2bfba4ddb9a35690d7ba
AndjelaMijailovic <AndjelaMijailovicAndja@hotmail.com>
2021-09-16 17:12:56 +0200

    issue with exported data has been fixed

--- AnnoPI/js/scriptFile.js
+++ AnnoPI/js/scriptFile.js
@@ -7,7 +7,7 @@ var buttonCommon = {
         exportOptions: {
             format: {
 						body: function ( data, row, column, node ) {
-									return column == 9 || column == 10 ? ( node.getElementsByTagName("a") != undefined ? node.getElementsByTagName("a")[0].href : "" ): (column == 7 || column == 8 ? node.textContent.replace("<br />", " ") : node.textContent);
+									return column == 9 || column == 10 ? ( node.getElementsByTagName("a")[0] != undefined ? node.getElementsByTagName("a")[0].href : "" ): (column == 7 || column == 8 ? node.textContent.replace("<br />", " ") : node.textContent);
 						}					
             },
 			orthogonal: 'export',
@@ -31,7 +31,7 @@ var table = $('#annotationTable').DataTable({
 								format: {
 								body: function ( data, row, column, node ) {
 
-								return column == 9 || column == 10 ? (node.getElementsByTagName("a") != undefined ? node.getElementsByTagName("a")[0].href : ""): (column == 7 || column == 8 ? node.textContent.replace("<br />", " ") : node.textContent);
+								return column == 9 || column == 10 ? (node.getElementsByTagName("a")[0] != undefined ? node.getElementsByTagName("a")[0].href : ""): (column == 7 || column == 8 ? node.textContent.replace("<br />", " ") : node.textContent);
 								}
 								}
                             },
